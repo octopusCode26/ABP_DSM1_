e0c48daae6625a724bf81d4382657edbde237b0a
renanrmsantos14 <renanrmsantos14@gmail.com>
2026-05-05 20:36:13 -0300

    Revert "Revert "Revert "Revert "Revert "conserto da responsividade do index"""""
    This reverts commit 335dcaf4be7b74578f9d9b4433f303bf4d33441f.

---
+++ src/routes/questoes.routes.js
@@ -0,0 +1,30 @@
+const { Router } = require("express");
+const authMiddlewares = require("../middlewares/auth.middlewares");
+const router = Router();
+const { findProximaQuestaoByUsuario } = require("../repositories/questoes.repositories");
+
+/*
+curl -X GET http://localhost:3000/api/questoes/proxima-questao \
+-H "Authorization: Bearer SEU_TOKEN"
+*/
+
+router.get("/proxima-questao", authMiddlewares, async function (req, res) {
+  try {
+    const questao = await findProximaQuestaoByUsuario(req.usuario.id_usuario);
+
+    if (!questao) {
+      return res
+        .status(404)
+        .json({ message: "nenhuma questão pendente encontrada" });
+    }
+
+    return res.status(200).json(questao);
+} catch (e) {
+return res.status(500).json({
+message: "erro interno do servidor",
+});
+}
+});
+
+// exporta o "router" para outros arquivos.
+module.exports = router;
